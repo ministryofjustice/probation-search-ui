88cf2fb55cde214de943a52ca69fe9666caa8ec7
Marcus Aspin <maspin@unilink.com>
2023-09-27 09:03:45 +0100

    PI-1449 Add integration tests

---
+++ integration_tests/e2e/delius.cy.ts
@@ -0,0 +1,32 @@
+context('Delius search', () => {
+  beforeEach(() => {
+    cy.task('reset')
+    cy.task('stubSignIn')
+    cy.task('stubAuthUser')
+  })
+
+  it('displays welcome message', () => {
+    cy.signIn()
+    cy.visit('/delius/nationalSearch')
+    cy.get('main').should('contain.text', 'Find people by using any combination of:')
+  })
+
+  it('displays results', () => {
+    cy.signIn()
+    cy.visit('/delius/nationalSearch')
+    cy.get('#search').type('test')
+    cy.get('#search-results-container')
+      .should('contain.text', 'Showing 1 to 2 of 2 results.')
+      .should('contain.text', 'A000001')
+      .should('contain.text', 'A000002')
+      .should('contain.text', 'London')
+      .should('contain.text', 'Practitioner, Probation')
+  })
+
+  it('navigates to help page', () => {
+    cy.signIn()
+    cy.visit('/delius/nationalSearch')
+    cy.get('#help-link').click()
+    cy.get('h1').should('contain.text', 'Search tips')
+  })
+})
