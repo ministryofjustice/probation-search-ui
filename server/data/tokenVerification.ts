15ccdde5960355121ddf1f11af7044976ea9b762
Marcus Aspin <maspin@unilink.com>
2023-09-27 17:43:30 +0100

    Added test

--- server/data/tokenVerification.ts
+++ server/data/tokenVerification.ts
@@ -20,7 +20,7 @@ export type TokenVerifier = (request: Request) => Promise<boolean | void>
 const tokenVerifier: TokenVerifier = async request => {
   const { user, verified } = request
 
-  if (!config.apis.tokenVerification.enabled || request.path.startsWith('/delius/nationalSearch')) {
+  if (!config.apis.tokenVerification.enabled || request.path?.startsWith('/delius/nationalSearch')) {
     logger.debug('Token verification disabled, returning token is valid')
     return true
   }
