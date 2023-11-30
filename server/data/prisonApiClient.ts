27b4ef7056171fbbc44ac9f28975acec87b8f794
Marcus Aspin <maspin@unilink.com>
2023-11-30 11:01:55 +0000

    PI-1697 Fall back to placeholder image on Prison API failure

--- server/data/prisonApiClient.ts
+++ server/data/prisonApiClient.ts
@@ -1,5 +1,6 @@
 import config from '../config'
 import RestClient from './restClient'
+import logger from '../../logger'
 
 export default class PrisonApiClient extends RestClient {
   constructor(token: string) {
@@ -10,11 +11,16 @@ export default class PrisonApiClient extends RestClient {
     const placeholder = '/assets/images/NoPhoto@2x.png'
     if (nomsNumber === undefined) return placeholder
 
-    const imageData = await this.get({
-      path: `/api/bookings/offenderNo/${nomsNumber}/image/data`,
-      responseType: 'blob',
-      handle404: true,
-    })
-    return imageData ? `data:image/jpeg;charset=utf-8;base64,${(imageData as Buffer).toString('base64')}` : placeholder
+    try {
+      const imageData = await this.get({
+        path: `/api/bookings/offenderNo/${nomsNumber}/image/data`,
+        responseType: 'blob',
+        handle404: true,
+      })
+      if (imageData) return `data:image/jpeg;charset=utf-8;base64,${(imageData as Buffer).toString('base64')}`
+    } catch (error) {
+      logger.error('Unable to fetch prisoner image, fallback to placeholder', error)
+    }
+    return placeholder
   }
 }
