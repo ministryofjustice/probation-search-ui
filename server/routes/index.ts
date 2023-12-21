a42aad8c6214a78d351d2a1602029143c5b0aac5
Marcus Aspin <maspin@unilink.com>
2023-12-21 15:37:11 +0000

    PI-1754 Store filter values (providers, match all terms) in the session
    Also updated to always display selected providers, even if they don't appear in the result aggregations

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -54,6 +54,14 @@ export default function routes(service: Services): Router {
     oauthClient: service.hmppsAuthClient,
   })
 
+  router.post('/delius/nationalSearch/filters', (req, res) => {
+    if (!req.session.probationSearch) req.session.probationSearch = {}
+    const session = req.session.probationSearch
+    session.matchAllTerms = req.body.matchAllTerms
+    session.providers = req.body.providers
+    return res.sendStatus(200)
+  })
+
   router.get('/delius/nationalSearch/help', (req, res) => res.render('pages/deliusSearch/help'))
 
   router.post('/delius/nationalSearch/trackEvent', (req, res) => {
@@ -72,6 +80,18 @@ async function mapResults(
   ApplicationInsightsEvents.searchPerformed(request, response)
   const token = await hmppsAuthClient.getSystemClientToken()
   const prisonApiClient = new PrisonApiClient(token)
+  const returnedProviders = response.probationAreaAggregations.map(p => ({
+    value: `${p.code}-${p.description}`,
+    text: `${p.description} (${p.count})`,
+    checked: request.providersFilter.includes(`${p.code}-${p.description}`),
+  }))
+  const selectedProviders = request.providersFilter
+    .filter(p => !returnedProviders.find(r => r.value === p))
+    .map(p => ({
+      value: p,
+      text: `${p.substring(4)} (0)`,
+      checked: true,
+    }))
   return {
     results: await Promise.all(
       response.content.map(async result => {
@@ -86,13 +106,7 @@ async function mapResults(
       }),
     ),
     query: request.query,
-    providers: response.probationAreaAggregations
-      .map(p => ({
-        value: p.code,
-        text: `${p.description} (${p.count})`,
-        checked: request.providersFilter.includes(p.code),
-      }))
-      .sort((a, b) => a.text?.localeCompare(b.text)),
+    providers: [...selectedProviders, ...returnedProviders].sort((a, b) => a.text?.localeCompare(b.text)),
     matchAllTerms: request.matchAllTerms,
     deliusUrl: config.delius.url,
   }
