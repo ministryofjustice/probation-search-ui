64d1583e86e355e2a65acb07548169a60f8583b7
Marcus Aspin <maspin@unilink.com>
2024-01-09 14:27:09 +0000

    PI-1763 Serve prisoner images separately to search results (#102)
    * PI-1763 Serve prisoner images separately to search results
    
    The URL is signed so that the URL can't be modified to retrieve any other prisoner's image.
    
    * Use sendBeacon for sending analytics
    
    This ensures the request is still sent even if the window is closed, and doesn't hold up the Delius navigation.
    
    * Fix Sonar issue

--- server/data/prisonApiClient.ts
+++ server/data/prisonApiClient.ts
@@ -1,26 +1,16 @@
+import { Readable } from 'stream'
 import config from '../config'
 import RestClient from './restClient'
-import logger from '../../logger'
 
 export default class PrisonApiClient extends RestClient {
   constructor(token: string) {
     super('PrisonApiClient', config.apis.prisonApi, token)
   }
 
-  async getImageUrl(nomsNumber?: string): Promise<string> {
-    const placeholder = '/assets/images/NoPhoto@2x.png'
-    if (nomsNumber === undefined) return placeholder
-
-    try {
-      const imageData = await this.get({
-        path: `/api/bookings/offenderNo/${nomsNumber}/image/data`,
-        responseType: 'blob',
-        handle404: true,
-      })
-      if (imageData) return `data:image/jpeg;charset=utf-8;base64,${(imageData as Buffer).toString('base64')}`
-    } catch (error) {
-      logger.error('Unable to fetch prisoner image, fallback to placeholder', error)
-    }
-    return placeholder
+  async getImageData(nomsNumber: string): Promise<Readable> {
+    return (await this.stream({
+      path: `/api/bookings/offenderNo/${nomsNumber}/image/data`,
+      handle404: true,
+    })) as Promise<Readable>
   }
 }
