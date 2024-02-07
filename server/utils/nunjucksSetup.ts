1c86223b57d2f6a328f2816e50d6caefb995d57c
Anthony Britton <anthony.britton@digital.justice.gov.uk>
2024-02-07 16:15:37 +0000

    PI-1850 update to frontend v5

--- server/utils/nunjucksSetup.ts
+++ server/utils/nunjucksSetup.ts
@@ -31,7 +31,7 @@ export default function nunjucksSetup(app: express.Express, applicationInfo: App
       path.join(__dirname, '../../server/views'),
       'node_modules/govuk-frontend/dist',
       'node_modules/govuk-frontend/dist/components/',
-      'node_modules/@ministryofjustice/frontend/d',
+      'node_modules/@ministryofjustice/frontend/',
       'node_modules/@ministryofjustice/frontend/moj/components/',
       'node_modules/@ministryofjustice/probation-search-frontend/components',
     ],
