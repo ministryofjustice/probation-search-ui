a19e54c40ea91d0c12245352b0be7ab016bfe6bc
Paul McPhee <paul.mcphee@digital.justice.gov.uk>
2024-02-14 15:24:41 +0000

    PI-1836: Added hmpps audit

--- server/routes/case.ts
+++ server/routes/case.ts
@@ -1,5 +1,21 @@
-import { Router } from 'express'
+import { type RequestHandler, Router } from 'express'
+import { auditService } from '@ministryofjustice/hmpps-audit-client'
+import { v4 } from 'uuid'
+import asyncMiddleware from '../middleware/asyncMiddleware'
 
 export default function caseViewRoutes(router: Router) {
-  router.get('/case/:crn', (req, res) => res.render('pages/case', { crn: req.params.crn }))
+  const get = (path: string | string[], handler: RequestHandler) => router.get(path, asyncMiddleware(handler))
+
+  get('/case/:crn', async (req, res, _next) => {
+    const { crn } = req.params
+    await auditService.sendAuditMessage({
+      action: 'VIEW_CASE_DETAILS',
+      who: res.locals.user.username,
+      subjectId: crn,
+      subjectType: 'CRN',
+      correlationId: v4(),
+      service: 'probation-search-ui',
+    })
+    res.render('pages/case', { crn })
+  })
 }
