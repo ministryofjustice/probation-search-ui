d611c904ffcb879db4a85f2f326387a0ce1f7726
Marcus Aspin <maspin@unilink.com>
2024-10-08 16:24:52 +0100

    PI-2561 Use includeScores flag (#218)

--- server/data/contactSearchClient.ts
+++ server/data/contactSearchClient.ts
@@ -48,6 +48,7 @@ export default class ContactSearchApiClient extends RestClient {
         crn,
         query,
         matchAllTerms: false,
+        includeScores: true,
       },
     })) as Page<Contact>
 
